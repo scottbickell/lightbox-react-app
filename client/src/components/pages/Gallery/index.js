0c8afbe8725f5a2bb963d7140ce3013a4723a045
Scott Bickell <bickellworks@gmail.com>
2019-04-04 21:13:57 -0600

    Add gallery files

--- client/src/components/pages/Gallery/index.js
+++ client/src/components/pages/Gallery/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import data from "../../../photoData.json";
 
 import "./styles.css";
 
@@ -7,7 +8,12 @@ class Gallery extends Component {
 
     return (
         <div>
-          <h1>Gallery</h1>
+          <h2>Gallery</h2>
+          {data.map(d => (
+            // <div style="background-image:url(http://placekitten.com/100/200)"></div>
+            // <div className="photo-container" style="background-image:url("");"></div>
+            <img src={"uploads/" + d.photoFileName} className="photo" />
+          ))}
         </div>
     );
   }
